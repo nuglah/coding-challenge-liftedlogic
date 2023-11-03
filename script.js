eaacd99397d9571cb0c7ab506d04c7a48648227f
Ryan <thisianemailaddressthatis@gmail.com>
2023-11-02 20:02:16 -0500

    added new releases

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ document.getElementById("nav-toggle").addEventListener("click", function() {
     var navList = document.querySelector(".nav-list");
     navList.classList.toggle("active");
 });
+
+$(document).ready(function () {
+	$(".slick-carousel").slick({
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		arrows: true,
+		infinite: true,
+		autoplay: false,
+        centerMode: true,
+        focusOnSelect: true,
+	});
+	$(".prev-btn").click(function () {
+		$(".slick-carousel").slick("slickPrev");
+	});
+
+	$(".next-btn").click(function () {
+		$(".slick-carousel").slick("slickNext");
+	});
+	$(".prev-btn").addClass("slick-disabled");
+});
+
+
+
